14342139bb2750a3e2683bdef3213f069f2d238a
Ethan <ethangroene@gmail.com>
2023-08-14 19:12:57 -0400

    Add initial solution for class component

--- src/Components/Class/ClassApp.jsx
+++ src/Components/Class/ClassApp.jsx
@@ -1,21 +1,74 @@
 import { Component } from "react";
 import { ClassScoreBoard } from "./ClassScoreBoard";
-import { ClassGameBoard } from "./ClassGameBoard";
+import { ClassGameBoard, initialFishes, allFishNames } from "./ClassGameBoard";
 import { ClassFinalScore } from "./ClassFinalScore";
 
 export class ClassApp extends Component {
-  state = {
-    incorrectCount: 0,
-    correctCount: 0,
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentIndex: 0,
+      incorrectCount: 0,
+      correctCount: 0,
+      isQuizComplete: false,
+      currentInput: "",
+      answersLeft: ["trout", "salmon", "tuna", "shark"], // must be in this order: "trout", "salmon", "tuna", "shark"
+    };
+  }
+
+  // To avoid prop drilling, declare methods that deal with parent's state values, here:
+  handleSubmission = (e) => {
+    e.preventDefault();
+
+    // I'm using setState() 2x in this method; is there a way to set the same conditions, but use it only once?
+    if (this.state.currentInput === allFishNames[this.state.currentIndex]) {
+      this.setState({ correctCount: this.state.correctCount + 1 });
+    } else {
+      this.setState({ incorrectCount: this.state.incorrectCount + 1 });
+    }
+
+    if (this.state.currentIndex !== initialFishes.length - 1) {
+      this.setState({
+        currentIndex: this.state.currentIndex + 1,
+        currentInput: "",
+        answersLeft: this.state.answersLeft.slice(1),
+      });
+    } else {
+      this.setState({ isQuizComplete: true });
+    }
   };
+
+  handleInput = (e) => {
+    let value = e.target.value.trim().toLowerCase();
+    this.setState({ currentInput: value });
+  };
+
   render() {
     return (
       <>
         <>
-          <ClassScoreBoard />
-          <ClassGameBoard />
+          {!this.state.isQuizComplete && (
+            <ClassScoreBoard
+              answersLeft={this.state.answersLeft}
+              incorrectCount={this.state.incorrectCount}
+              correctCount={this.state.correctCount}
+            />
+          )}
+          {!this.state.isQuizComplete && (
+            <ClassGameBoard
+              handleInput={this.handleInput}
+              currentIndex={this.state.currentIndex}
+              currentInput={this.state.currentInput}
+              handleSubmission={this.handleSubmission}
+            />
+          )}
         </>
-        {false && <ClassFinalScore />}
+        {this.state.isQuizComplete && (
+          <ClassFinalScore
+            correctCount={this.state.correctCount}
+            totalCount={initialFishes.length}
+          />
+        )}
       </>
     );
   }
--- src/Components/Class/ClassGameBoard.jsx
+++ src/Components/Class/ClassGameBoard.jsx
@@ -2,7 +2,7 @@ import { Component } from "react";
 import "./styles/game-board.css";
 import { Images } from "../../assets/Images";
 
-const initialFishes = [
+export const initialFishes = [
   {
     name: "trout",
     url: Images.trout,
@@ -21,18 +21,35 @@ const initialFishes = [
   },
 ];
 
+export const allFishNames = initialFishes.map((fish) => fish.name);
+
 export class ClassGameBoard extends Component {
   render() {
-    const nextFishToName = initialFishes[0];
+    const { handleSubmission, currentIndex, currentInput, handleInput } =
+      this.props;
+
+    const currentFish = initialFishes[currentIndex];
+
     return (
       <div id="game-board">
         <div id="fish-container">
-          <img src={nextFishToName.url} alt={nextFishToName.name} />
+          <img src={currentFish.url} alt={currentFish.name} />
         </div>
         <form id="fish-guess-form">
           <label htmlFor="fish-guess">What kind of fish is this?</label>
-          <input type="text" name="fish-guess" />
-          <input type="submit" />
+          <input
+            value={currentInput}
+            onChange={handleInput}
+            type="text"
+            name="fish-guess"
+          />
+          {/* Only enable submit as long as array of fish names includes input. Or, if exactly-like-model requirement is strict, allow for non-answers */}
+          {/* Clear input when moving to next question */}
+          <input
+            disabled={!allFishNames.includes(currentInput)}
+            onClick={handleSubmission}
+            type="submit"
+          />
         </form>
       </div>
     );
