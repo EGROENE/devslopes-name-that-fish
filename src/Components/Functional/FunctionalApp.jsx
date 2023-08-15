8eaee6f92d5c830cb60996a0517050cd7c80c5eb
Ethan <ethangroene@gmail.com>
2023-08-14 22:52:29 -0400

    Add initial solution for Functional component

--- src/Components/Functional/FunctionalApp.jsx
+++ src/Components/Functional/FunctionalApp.jsx
@@ -1,13 +1,68 @@
 import { FunctionalGameBoard } from "./FunctionalGameBoard";
 import { FunctionalScoreBoard } from "./FunctionalScoreBoard";
 import { FunctionalFinalScore } from "./FunctionalFinalScore";
+import { useState } from "react";
+import { initialFishes } from "../../constants.js";
 
 export function FunctionalApp() {
+  let [currentIndex, nextIndex] = useState(0);
+  let [incorrectCount, setIncorrectCount] = useState(0);
+  let [correctCount, setCorrectCount] = useState(0);
+  let [isQuizComplete, setIsQuizComplete] = useState(false);
+  let [currentInput, setCurrentInput] = useState("");
+  let [answersLeft, setAnswersLeft] = useState([
+    "trout",
+    "salmon",
+    "tuna",
+    "shark",
+  ]);
+
+  const handleSubmission = (e) => {
+    e.preventDefault();
+
+    if (initialFishes[currentIndex].name === currentInput) {
+      setCorrectCount(correctCount + 1);
+    } else {
+      setIncorrectCount(incorrectCount + 1);
+    }
+
+    if (currentIndex !== initialFishes.length - 1) {
+      nextIndex(currentIndex + 1);
+      setCurrentInput("");
+      setAnswersLeft(answersLeft.slice(1));
+    } else {
+      setIsQuizComplete(true);
+    }
+  };
+
+  const handleInput = (e) => {
+    let value = e.target.value.trim().toLowerCase();
+    setCurrentInput(value);
+  };
+
   return (
     <>
-      <FunctionalScoreBoard />
-      <FunctionalGameBoard />
-      {false && <FunctionalFinalScore />}
+      {!isQuizComplete && (
+        <FunctionalScoreBoard
+          incorrectCount={incorrectCount}
+          correctCount={correctCount}
+          answersLeft={answersLeft}
+        />
+      )}
+      {!isQuizComplete && (
+        <FunctionalGameBoard
+          currentIndex={currentIndex}
+          handleInput={handleInput}
+          handleSubmission={handleSubmission}
+          currentInput={currentInput}
+        />
+      )}
+      {isQuizComplete && (
+        <FunctionalFinalScore
+          correctCount={correctCount}
+          totalCount={initialFishes.length}
+        />
+      )}
     </>
   );
 }
--- src/Components/Functional/FunctionalGameBoard.jsx
+++ src/Components/Functional/FunctionalGameBoard.jsx
@@ -1,36 +1,34 @@
 import "./styles/game-board.css";
-import { Images } from "../../assets/Images";
+import { initialFishes } from "../../constants";
 
-const initialFishes = [
-  {
-    name: "trout",
-    url: Images.trout,
-  },
-  {
-    name: "salmon",
-    url: Images.salmon,
-  },
-  {
-    name: "tuna",
-    url: Images.tuna,
-  },
-  {
-    name: "shark",
-    url: Images.shark,
-  },
-];
+export function FunctionalGameBoard({
+  currentIndex,
+  handleSubmission,
+  currentInput,
+  handleInput,
+}) {
+  const currentFish = initialFishes[currentIndex];
 
-export function FunctionalGameBoard() {
-  const nextFishToName = initialFishes[0];
+  const allFishNames = initialFishes.map((fish) => fish.name);
+
   return (
     <div id="game-board">
       <div id="fish-container">
-        <img src={nextFishToName.url} alt={nextFishToName.name} />
+        <img src={currentFish.url} alt={currentFish.name} />
       </div>
       <form id="fish-guess-form">
         <label htmlFor="fish-guess">What kind of fish is this?</label>
-        <input type="text" name="fish-guess" />
-        <input type="submit" />
+        <input
+          value={currentInput}
+          onChange={handleInput}
+          type="text"
+          name="fish-guess"
+        />
+        <input
+          disabled={!allFishNames.includes(currentInput)}
+          onClick={handleSubmission}
+          type="submit"
+        />
       </form>
     </div>
   );
